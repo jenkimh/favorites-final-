c12314fcb020077958b4043beb9b6c205b97c0a6
Jeny Kim <jeny@jenys-mbp.mynetworksettings.com>
2024-11-04 14:33:39 -0500

    fav color

--- questions.py
+++ questions.py
@@ -11,7 +11,7 @@
 """
 
 first_question = "What is your favorite color?"
-first_question_answer = ""
+first_question_answer = "Green"
 
 second_question = "What is your favorite food?"
 second_question_answer = ""
