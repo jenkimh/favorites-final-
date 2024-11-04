4e552cb64c4653b9668093ad557c7043b182e592
Jeny Kim <jeny@jenys-mbp.mynetworksettings.com>
2024-11-04 14:32:38 -0500

    no answers

--- questions.py
+++ questions.py
@@ -1,26 +1,26 @@
 # Welcome to the final Git exercise.
 # We're in a Python file again, but it looks like
-# we've been beaten to the punch! Please 
+# we've been beaten to the punch! Please
 # follow the instructions in Learn to get the repository
 # into a state where you can fill in your own answers!
 
 """
-!!!!! 
+!!!!!
   DO NOT MODIFY ANY OF THE CODE BELOW THIS LINE
 !!!!!
 """
 
 first_question = "What is your favorite color?"
-first_question_answer = "Chartreuse"
+first_question_answer = ""
 
 second_question = "What is your favorite food?"
-second_question_answer = "Foie gras"
+second_question_answer = ""
 
 third_question = "Who is your favorite fictional character? ex. Mickey Mouse, Bugs Bunny"
-third_question_answer = "Scrooge McDuck"
+third_question_answer = ""
 
 fourth_question = "What is your favorite animal?"
-fourth_question_answer = "Chinchilla"
+fourth_question_answer = ""
 
 fifth_question = "What is your favorite programming language? (Hint: You can always say Python!!)"
-fifth_question_answer = "COBOL"
+fifth_question_answer = ""
